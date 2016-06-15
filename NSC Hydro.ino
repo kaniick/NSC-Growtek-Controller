2b461ae3b98df37f00f2aa975a571ae5cce8e8dd
Nick <kaniick@users.noreply.github.com>
2016-06-14 20:16:08 -0700

    Update NSC Hydro.ino

--- NSC Hydro.ino
+++ NSC Hydro.ino
@@ -24,7 +24,7 @@
 // LEGAL DISCLAIMER:
 //   Growtek Hydroponics Controller v.1.0, Copyright 2015-2016 Nick J Penna.
 //   I'm providing this program as free software with the sole intent on furthering
-//   DIY Aquarium Lighting, but WITHOUT ANY WARRANTY; without even the implied warranty
+//   DIY Hydroponics, but WITHOUT ANY WARRANTY; without even the implied warranty
 //   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You may modify and/or use
 //   it under the terms of the GNU General Public License as published by the Free
 //   Software Foundation version 3 of the License, or (at your option) any later
